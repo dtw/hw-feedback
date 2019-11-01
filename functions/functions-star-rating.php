7fed66c4f6f5c23f486f5017c4e923235bd17b85
dtw <dtw@users.github.com>
2019-11-01 12:14:12 +0000

    Add function code

--- functions/functions-star-rating.php
+++ functions/functions-star-rating.php
@@ -10,6 +10,39 @@
  * Description: Takes three arguments to output star ratings for hw-feedback
  */
 
-  function feedbackstarrating($type,$float,$colour)
-
+  function feedbackstarrating($float,$type = 'individual',$colour = NULL) {
+    // check the type is valid
+    if ( $type !== 'individual' or $type !== 'average' ) {
+      die("Invalid type argument");
+    }
+    // has a colour been passed?
+    if ( isset($colour)) {
+      // add a leading space
+      $colour = ' '.$colour
+      $colour_class = true;
+    }
+    // output the stars
+    // we never want less than one star
+		if ($float < 1) {
+			echo '<i class="fas fa-star fa-lg' . if ($colour_class) {echo $colour;} . '"></i>';
+		} else {
+      // output whole stars based on the integer value of float
+			for ($int_count = 1; $int_count <= floor($float); $int_count++) {
+				echo '<i class="fas fa-star fa-lg' . if ($colour_class) {echo $colour;} . '"></i>';
+				$star_count++;
+			}
+      // if type is average, check if half a star needed
+      if ( $type === 'average' ) {
+  			if (($float - floor($float)) >= 0.25 && $float < 0.75) {
+  				echo '<i class="fa fa-star-half-empty fa-lg' . if ($colour_class) {echo $colour;} . '"></i>';
+  				$star_count++;
+  			}
+      }
+      // add empty stars up to 5
+			while ($star_count < 5) {
+				echo '<i class="far fa-star fa-lg' . if ($colour_class) {echo $colour;} . '"></i>';
+				$star_count++;
+			}
+		}
+  }
 ?>
