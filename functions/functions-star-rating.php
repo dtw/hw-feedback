f8853afad7f7c161a08ac9c4be5f647561af9c09
dtw <dtw@users.github.com>
2019-11-01 11:49:46 +0000

    Create functions-star-rating.php

---
+++ functions/functions-star-rating.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Output feedback star rating
+ *
+ * @package   hw-feedback
+ * @author    Phil Thiselton <dibblethewrecker@gmail.com>
+ * @license   GPL-2.0+
+ * @copyright 2019 Phil Thiselton
+ *
+ * Description: Takes three arguments to output star ratings for hw-feedback
+ */
+
+  function feedbackstarrating($type,$float,$colour)
+
+?>
