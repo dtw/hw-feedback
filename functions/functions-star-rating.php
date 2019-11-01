d03feb734725c69ed6c26a92f0e15bfb09b0283e
dtw <dtw@users.github.com>
2019-11-01 12:32:43 +0000

    Remove if statements

--- functions/functions-star-rating.php
+++ functions/functions-star-rating.php
@@ -19,28 +19,27 @@
     if ( isset($colour)) {
       // add a leading space
       $colour = ' '.$colour
-      $colour_class = true;
     }
     // output the stars
     // we never want less than one star
 		if ($float < 1) {
-			echo '<i class="fas fa-star fa-lg' . if ($colour_class) {echo $colour;} . '"></i>';
+			echo '<i class="fas fa-star fa-lg' . $colour . '"></i>';
 		} else {
       // output whole stars based on the integer value of float
 			for ($int_count = 1; $int_count <= floor($float); $int_count++) {
-				echo '<i class="fas fa-star fa-lg' . if ($colour_class) {echo $colour;} . '"></i>';
+				echo '<i class="fas fa-star fa-lg' . $colour . '"></i> ';
 				$star_count++;
 			}
       // if type is average, check if half a star needed
       if ( $type === 'average' ) {
   			if (($float - floor($float)) >= 0.25 && $float < 0.75) {
-  				echo '<i class="fa fa-star-half-empty fa-lg' . if ($colour_class) {echo $colour;} . '"></i>';
+  				echo '<i class="fa fa-star-half-empty fa-lg' . $colour . '"></i> ';
   				$star_count++;
   			}
       }
       // add empty stars up to 5
 			while ($star_count < 5) {
-				echo '<i class="far fa-star fa-lg' . if ($colour_class) {echo $colour;} . '"></i>';
+				echo '<i class="far fa-star fa-lg' . $colour . '"></i> ';
 				$star_count++;
 			}
 		}
