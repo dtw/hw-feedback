4d9960b46ff7ac64cfa0006a1e32f7c26518272d
dtw <dtw@users.github.com>
2019-11-11 11:37:01 +0000

    Create functions-get-rating.php

---
+++ functions/functions-get-rating.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Output feedback star rating
+ *
+ * @package   hw-feedback
+ * @author    Phil Thiselton <dibblethewrecker@gmail.com>
+ * @license   GPL-2.0+
+ * @copyright 2019 Phil Thiselton
+ *
+ * Description: Takes a Post ID and returns the ratings count and ratings total
+ * @param int $postID
+ */
+
+  function getrating($postID) {
+    
+  }
+?>
