49889fcb712c86d10c9b1af424b38185c4965f6e
dtw <dtw@users.github.com>
2020-01-02 17:09:18 +0000

    Switch from echo to return

--- functions/functions-star-rating.php
+++ functions/functions-star-rating.php
@@ -10,6 +10,7 @@
  * Description: Takes three arguments to output star ratings for hw-feedback
  * @param float $rating Takes any number
  * @param string $colour A colour to pass as a CSS class e.g. 'green'
+ * @param string $size A size corresponding to fontawesome styles https://fontawesome.com/how-to-use/on-the-web/styling/sizing-icons
  */
 
   function feedbackstarrating($rating,$colour = '') {
@@ -21,27 +22,29 @@
       $colour = ' '.$colour;
     }
     // output the stars
+    $output .= '';
     // we never want less than one star
 		if ($rating < 1) {
-			echo '<i class="fas fa-star fa-lg' . $colour . '"></i>';
+			$output .= '<i class="fas fa-star fa-lg' . $colour . '"></i>';
 		} else {
       // output whole stars based on the integer value of float
 			for ($int_count = 1; $int_count <= floor($rating); $int_count++) {
-				echo '<i class="fas fa-star fa-lg' . $colour . '"></i> ';
+				$output .= '<i class="fas fa-star fa-lg' . $colour . '"></i> ';
 				$star_count++;
 			}
       // if rating is average, check if half a star needed
       if ( $average) {
   			if (($rating - floor($rating)) >= 0.25 && ($rating - floor($rating)) < 0.75) {
-  				echo '<i class="fas fa-star-half-alt fa-lg' . $colour . '"></i> ';
+  				$output .= '<i class="fas fa-star-half-alt fa-lg' . $colour . '"></i> ';
   				$star_count++;
   			}
       }
       // add empty stars up to 5
 			while ($star_count < 5) {
-				echo '<i class="far fa-star fa-lg' . $colour . '"></i> ';
+				$output .= '<i class="far fa-star fa-lg' . $colour . '"></i> ';
 				$star_count++;
 			}
 		}
+  return $output;
   }
 ?>
