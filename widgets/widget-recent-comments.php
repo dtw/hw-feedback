7b031fa239165e1ab1b7eb03d85777e2191bd03f
dtw <dtw@users.noreply.github.com>
2020-01-28 17:22:41 +0000

    Fix mostly whitespace
    Leading to...

--- widgets/widget-recent-comments.php
+++ widgets/widget-recent-comments.php
@@ -44,72 +44,51 @@ $comments = $comments_query->query( $args );
 if ( $comments ) {
 
 echo "<div class='row'>";
-
-
 	foreach ( $comments as $comment ) { ?>
-
-
-<div class="review-container col-md-3 col-sm-6 col-xs-12">
-
-
-
-<?php												// Display icon for taxonomy term
-
-$term_ids = get_the_terms( $comment->comment_post_ID, 'service_types' );	// Find taxonomies
-$term_id = $term_ids[0]->term_id;											// Get taxonomy ID
-$term_name = $term_ids[0]->name;											// Get taxonomy name
-$term_icon = get_term_meta( $term_id, 'icon', true );						// Get meta
+	<div class="review-container col-md-3 col-sm-6 col-xs-12">
+		<?php												// Display icon for taxonomy term
+			$term_ids = get_the_terms( $comment->comment_post_ID, 'service_types' );	// Find taxonomies
+			$term_id = $term_ids[0]->term_id;											// Get taxonomy ID
+			$term_name = $term_ids[0]->name;											// Get taxonomy name
+			$term_icon = get_term_meta( $term_id, 'icon', true );						// Get meta
 		?>
 
-	<div class="text-center"><a class="img-anchor" href="<?php echo get_the_permalink($comment->comment_post_ID); ?>" aria-hidden="true">
-    <img width="80" height="80" src="<?php echo $term_icon; ?>" alt="<?php echo $term_name?>"  />
-    </a></div>
-
-
-
-
-
-
-	<h3><a href="<?php echo get_the_permalink($comment->comment_post_ID); ?>"><?php echo get_the_title($comment->comment_post_ID); ?></a></h3>
-
-
-
-
-<?php // Display star rating
-$individual_rating = get_comment_meta( $comment->comment_ID, 'feedback_rating', true ); ?>
-
-<?php if ($individual_rating) { ?>
-
-    <p class="star-rating p-rating">
-    	<?php echo feedbackstarrating($individual_rating,array('size' => 'fa-lg'));
+		<div class="text-center">
+			<a class="img-anchor" href="<?php echo get_the_permalink($comment->comment_post_ID); ?>" aria-hidden="true">
+				<img width="80" height="80" src="<?php echo $term_icon; ?>" alt="<?php echo $term_name?>"  />
+			</a>
+		</div>
+		<h3>
+			<a href="<?php echo get_the_permalink($comment->comment_post_ID); ?>"><?php echo get_the_title($comment->comment_post_ID); ?></a>
+		</h3>
+		<?php // Display star rating
+		$individual_rating = get_comment_meta( $comment->comment_ID, 'feedback_rating', true );
+		if ($individual_rating) { ?>
+	  <p class="star-rating p-rating">
+	  	<?php echo feedbackstarrating($individual_rating,array('size' => 'fa-lg'));
 				if ($individual_rating == 1) echo '<span class="screen-reader-text">'.$individual_rating.' star</span>';
 				else echo '<span class="screen-reader-text">'.$individual_rating.' stars</span>';
 			?>
-    </p>
+	  </p>
 		<p>
 			<strong>
 				<?php echo human_time_diff( strtotime($comment->comment_date), current_time( 'timestamp' ) ); ?> ago
 			</strong>
 		</p>
-<?php } // end of if there is a rating ?>
-
+		<?php } // end of if there is a rating ?>
 		<p><?php // mb_strimwidth trims comment to 300 (if needed) and adds an ellipsis
 			// wpautop converts double line breaks to <p></p>
 			// i.e. this keeps line breaks in the comment
 			echo wpautop(mb_strimwidth($comment->comment_content,0,300," ..."), true); ?>
 		</p>
-
-
-
-<?php if (get_comment_meta( $comment->comment_ID, 'feedback_response', true )) { ?>
-	<div class="feedback-response">
-    <img width="100" height="100" class="alignright" src="<?php bloginfo(url) ?>/wp-content/themes/scaffold/images/icons/colour/response-small.png" alt="Response" />
-	<p><?php echo get_the_title($comment->comment_post_ID); ?> has responded to this feedback:</p>
-	<blockquote><em><?php echo mb_strimwidth ( get_comment_meta( $comment->comment_ID, 'feedback_response', true ),0,180," ..." ); ?></em>	<a href="<?php echo get_the_permalink($comment->comment_post_ID); ?>">read more</a></blockquote>
-</div><!-- end of response -->
-	<?php } ?>
-
-			</div><!-- end of col -->
+		<?php if (get_comment_meta( $comment->comment_ID, 'feedback_response', true )) { ?>
+			<div class="feedback-response">
+				<img width="100" height="100" class="alignright" src="<?php bloginfo(url) ?>/wp-content/themes/scaffold/images/icons/colour/response-small.png" alt="Response" />
+				<p><?php echo get_the_title($comment->comment_post_ID); ?> has responded to this feedback:</p>
+				<blockquote><em><?php echo mb_strimwidth ( get_comment_meta( $comment->comment_ID, 'feedback_response', true ),0,180," ..." ); ?></em>	<a href="<?php echo get_the_permalink($comment->comment_post_ID); ?>">read more</a></blockquote>
+			</div><!-- end of response -->
+		<?php } ?>
+	</div><!-- end of review-container -->
 
 <?php	 } // end of loop?
 
