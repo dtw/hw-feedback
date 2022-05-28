4e1b014e7186775a1830c3f83995a8e08e353d93
Phil T <362017+dtw@users.noreply.github.com>
2022-05-28 16:51:11 +0100

    Add js for location id change alert

--- js/copy_civicrm_subject_code.js
+++ js/copy_civicrm_subject_code.js
@@ -12,3 +12,11 @@ async function copy_civicrm_subject_code() {
 async function update_from_cqc($target, $source) {
   document.getElementById($target).value = document.getElementById($source).value;
 }
+
+jQuery(document).ready(function ($) {
+  $("#hw_services_cqc_location").change(function(){
+    if ($("#hw_services_cqc_location").value != ''){
+      $("#hw_services_cqc_location_alert").css("display", "inline-block");
+    }
+  });
+});
