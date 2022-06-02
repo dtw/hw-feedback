fc00dd361540a6ec879b3471caca367b62a7955e
Phil T <362017+dtw@users.noreply.github.com>
2022-06-02 15:07:16 +0100

    Change Closed to Archived
    Is more accurate to CQC terms

--- post-types-taxonomies.php
+++ post-types-taxonomies.php
@@ -151,7 +151,7 @@ function hw_feedback_register_default_terms () {
   wp_insert_term('Deregistered','cqc_reg_status', array('description'=>'Deregistered with CQC', 'slug' => 'deregistered'));
   wp_insert_term('Not registered','cqc_reg_status', array('description'=>'Not registered with CQC', 'slug' => 'not-registered'));
   wp_insert_term('Not applicable','cqc_reg_status', array('description'=>'Not required to register with CQC', 'slug' => 'not-applicable'));
-  wp_insert_term('Closed','cqc_reg_status', array('description'=>'No longer registered with CQC', 'slug' => 'closed'));
+  wp_insert_term('Archived','cqc_reg_status', array('description'=>'No longer registered with CQC', 'slug' => 'archived'));
 }
 
 add_action('wp_loaded', 'hw_feedback_register_default_terms');
