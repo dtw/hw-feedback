a15ffa5350a857e587b4845f6961933ad1a5d4a0
Phil T <362017+dtw@users.noreply.github.com>
2022-07-07 11:46:20 +0100

    Add js to show warnings when preview disabled

--- js/copy_civicrm_subject_code.js
+++ js/copy_civicrm_subject_code.js
@@ -13,10 +13,24 @@ async function update_from_cqc($target, $source) {
   document.getElementById($target).value = document.getElementById($source).value;
 }
 
+/* this is used on local_services edit page */
 jQuery(document).ready(function ($) {
   $("#hw-services-cqc-location").change(function(){
     if ($("#hw-services-cqc-location").value != ''){
       $("#hw-services-cqc-location-alert").css("display", "inline-block");
+    }
+  });
+});
+
+jQuery(document).ready(function ($) {
+  $("#hw-feedback-preview-only").change(function(){
+    if ($(this).is(":checked")) {
+      $("#hw-feedback-import-alert").css("display", "none");
+      $("#hw-feedback-form-submit").prop("value", "Preview");
+    }
+    if (!$(this).is(":checked")) {
+      $("#hw-feedback-import-alert").css("display", "inline-block");
+      $("#hw-feedback-form-submit").prop("value", "Import");
     }
   });
 });
