8527e205e10afa91d4ba041933a6e43ad642ebf1
Phil T <362017+dtw@users.noreply.github.com>
2022-07-08 16:04:02 +0100

    Add new shortcode for dual registration
    [dual_reg]1-10204617361[/dual_reg]
    
    Pending better handling in backend

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -51,4 +51,29 @@ function hwbucks_shortcode_review_callout( $atts, $content = null ) {
 
 add_shortcode( 'embed_review', 'hwbucks_shortcode_review_callout' );
 
+/* Media object DUAL REGISTRATION
+------------------------ */
+
+function hwbucks_shortcode_dual_registration_callout( $atts, $content = null ) {
+	$a = shortcode_atts( array(
+		'cqc_location_id' => 'RXQ32', // Location ID for the callout
+	), $atts );
+
+	if ( empty( $content ) ) {
+		$content = $a['cqc_location_id'];
+	}
+	$label = 'Dual registration';
+	$dual_reg_object = '
+	<div class="media callout callout-dual-reg">
+		<div class="media-left callout">
+				<i class="media-object fas fa-plus-square fa-2x shortcode-icon" aria-hidden="true" title="' . $label . '"></i>
+		</div>
+		<div class="media-body callout"><p>This location is run by two companies. They have a dual registration and are jointly responsible for the services. You can view the second registration on the CQC website: <a href="https://www.cqc.org.uk/location/' . $content . '">' . $content . '</a></p></div>
+	</div>';
+
+	return $dual_reg_object;
+}
+
+add_shortcode( 'dual_reg', 'hwbucks_shortcode_dual_registration_callout' );
+
 ?>
