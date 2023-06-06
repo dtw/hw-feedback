28354ce881e519af94eaa76abe36dcdd35496a7f
Phil T <362017+dtw@users.noreply.github.com>
2023-06-06 10:33:36 +0100

    Add functions-helper.php

---
+++ functions/functions-helper.php
@@ -0,0 +1,5 @@
+<?php
+
+/* Just some helpers */
+
+?>
--- healthwatchfeedback.php
+++ healthwatchfeedback.php
@@ -48,6 +48,7 @@ require_once(plugin_dir_path( __FILE__ ).'/functions/functions-get-rating.php');
 require_once(plugin_dir_path( __FILE__ ).'/functions/functions-cqc-api-query.php');
 require_once(plugin_dir_path( __FILE__ ).'/functions/functions-generate-metabox-form-fields.php');
 require_once(plugin_dir_path( __FILE__ ).'/functions/shortcodes.php');
+require_once(plugin_dir_path( __FILE__ ).'/functions/helper.php');
 
 // Enqueue CSS
 
