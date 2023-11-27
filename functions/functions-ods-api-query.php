eb7e45cf8289af39342f568c015ceb0a3cb4dbc4
Phil T <362017+dtw@users.noreply.github.com>
2023-11-27 12:41:50 +0000

    Add cqc to ods mapping

--- functions/functions-ods-api-query.php
+++ functions/functions-ods-api-query.php
@@ -17,7 +17,18 @@ function hw_feedback_ods_role_code_bootstrap($collection_name, $id)
   // check there are no ODS roles assigned to the service
 
   // if not, add ods_role_code based on cqc_inspection_category based on these pairs:
-
+  $cqc_to_ods_map = array(
+    'h1' => '198',
+    'h12' => '7',
+    'p1' => '110',
+    'p1' => '65',
+    'p2' => '76',
+    'p4' => '175',
+    'p6' => '87',
+    's1' => '269',
+    's2' => '104',
+    's2' => '270'
+  );
   // get each cqc_inspection_category, check against the list, add the ods_role_code taxonomy
 
 }
