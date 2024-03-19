36589620c45613f02a4ed31c9d870e495f862f29
Phil T <362017+dtw@users.noreply.github.com>
2024-03-19 17:00:59 +0000

    Version bump

--- healthwatchfeedback.php
+++ healthwatchfeedback.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Healthwatch Feedback
-Version: 2.8.4
+Version: 2.8.5
 Description: Implements a Rate and Review centre on Healthwatch websites. <strong>DO NOT DELETE !</strong>
 Author: Phil Thiselton & Jason King
 */
