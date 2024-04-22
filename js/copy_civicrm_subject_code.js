ccb9f36cf4d1a6343317ac0ff397a7669c656b91
Phil T. <362017+dtw@users.noreply.github.com>
2024-04-22 14:43:14 +0100

    Add copy_civicrm_uuid function

--- js/copy_civicrm_subject_code.js
+++ js/copy_civicrm_subject_code.js
@@ -1,3 +1,14 @@
+async function copy_civicrm_uuid() {
+  /* Get the text field */
+  var copyText = document.getElementById("civicrm-uuid-field");
+
+  /* Select the text field */
+  copyText.select();
+
+   /* Copy the text inside the text field */
+  await navigator.clipboard.writeText(copyText.value);
+}
+
 async function copy_civicrm_subject_code() {
   /* Get the text field */
   var copyText = document.getElementById("civicrm-subject-code-field");
